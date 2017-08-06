b989b68dae34e97ae46810f53a8639568ba861c9
HD <heightdifferential@gmail.com>
2017-08-05 22:55:23 -0500

    REV IMU stuff

--- HDCode/src/main/java/org/firstinspires/ftc/hdcode/HDSamples/TestAdafruit.java
+++ HDCode/src/main/java/org/firstinspires/ftc/hdcode/HDSamples/TestAdafruit.java
@@ -1,28 +1,41 @@
 package org.firstinspires.ftc.hdcode.HDSamples;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+import org.firstinspires.ftc.hdlib.OpModeManagement.HDOpMode;
 import org.firstinspires.ftc.hdlib.Sensors.AdafruitIMU;
 
 /**
  * Created by akash on 8/4/2017.
  */
 
-public class TestAdafruit extends OpMode{
-
+@Autonomous
+public class TestAdafruit extends HDOpMode{
     AdafruitIMU IMU1;
 
     @Override
-    public void init() {
+    public void initialize() {
         IMU1 = new AdafruitIMU("imu", 10);
     }
 
     @Override
-    public void loop() {
+    public void initializeLoop() {
+
+    }
+
+    @Override
+    public void Start() {
+
+    }
+
+    @Override
+    public void continuousRun(double elapsedTime) {
         telemetry.addData("Calibration Status", IMU1.getCalibrationStatus());
         telemetry.addData("X Heading: ", IMU1.getXheading());
         telemetry.addData("Y Heading: ", IMU1.getYheading());
         telemetry.addData("Z Heading: ", IMU1.getZheading());
+        telemetry.update();
     }
 
 
